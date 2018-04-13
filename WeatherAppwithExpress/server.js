a987519d0f917da6d7f899c9e0e82ec0e776413e
Srisarguru Sridhar <srisargurusridhar@u.boisestate.edu>
2018-04-12 22:54:46 -0400

    adding a simple ejs file to render the view at port 3000 20180412

--- WeatherAppwithExpress/server.js
+++ WeatherAppwithExpress/server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs') //set the view engine to ejs
+
 app.get('/', function (req, res) { //specifically focusing on the roor '/' URL
-  res.send('First hello world using express')
+  //res.send('First hello world using express')
+  res.render('index'); //rendering the index.ejs 
 })
 
 app.listen(3000, function () { //app listening on port 3000 for connections
