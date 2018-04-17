518d8e699954300180de16cf03a7c6630ebe5e3c
Srisarguru Sridhar <srisargurusridhar@u.boisestate.edu>
2018-04-16 22:12:53 -0400

    Added error check if city is not found - 20180416

--- WeatherAppwithExpress/server.js
+++ WeatherAppwithExpress/server.js
@@ -3,7 +3,7 @@ const app = express()
 const bodyParser = require('body-parser'); //getting body parser module
 const request = require('request');
 
-let apiKey ='1d41f23a07d1f8800592acdd9c1366d5';
+let apiKey = '1d41f23a07d1f8800592acdd9c1366d5';
 
 app.set('view engine', 'ejs') //set the view engine to ejs
 
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true })); //use the body parser to par
 
 app.get('/', function (req, res) { //specifically focusing on the roor '/' URL
   //res.send('First hello world using express')
-  res.render('index', {weather: null, error: null});; //rendering the index.ejs //added variables so that weather and error are not undefined while rendering to the browser
+  res.render('index', { weather: null, error: null });; //rendering the index.ejs //added variables so that weather and error are not undefined while rendering to the browser
 })
 
 //post handling
@@ -21,16 +21,18 @@ app.post('/temperature', function (req, res) {
   let city = req.body.city;
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
   request(url, function (err, response, body) { //make a get request to the weather API
-    if(err){
-      res.render('index', {weather: null, error: 'Oops! The weather man is on vacation. Please try again'});
+    if (err) {
+      res.render('index', { weather: null, error: 'Oops! The weather man is on vacation. Please try again' });
     } else {
       let weather = JSON.parse(body);
-      console.log(weather);
-      if(weather.main != undefined){
+      if (weather.cod == '404') { //added condition if city is not found
+        res.render('index', { weather: null, error: 'Cannot find city on Earth' });
+      }
+      else if (weather.main != undefined) {
         let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-        res.render('index', {weather: weatherText, error: null});
+        res.render('index', { weather: weatherText, error: null });
       } else {
-         res.render('index', {weather: null, error: 'Oops! The weather man is on vacation. Please try again'});
+        res.render('index', { weather: null, error: 'Oops! The weather man is on vacation. Please try again' });
       }
     }
   });
