bd7905b25a0dabc7698e8de9cf20c14d763710db
Srisarguru Sridhar <srisargurusridhar@u.boisestate.edu>
2018-04-19 22:19:23 -0400

    Added code to get humidity from open API - 20180419

--- WeatherAppwithExpress/server.js
+++ WeatherAppwithExpress/server.js
@@ -13,26 +13,32 @@ app.use(bodyParser.urlencoded({ extended: true })); //use the body parser to par
 
 app.get('/', function (req, res) { //specifically focusing on the roor '/' URL
   //res.send('First hello world using express')
-  res.render('index', { weather: null, error: null });; //rendering the index.ejs //added variables so that weather and error are not undefined while rendering to the browser
+  res.render('index', { weather: null, error: null, humidity: null }); //rendering the index.ejs //added variables so that weather and error are not undefined while rendering to the browser
 })
 
 //post handling
 app.post('/temperature', function (req, res) {
+  console.log(req.body);
   let city = req.body.city;
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
   request(url, function (err, response, body) { //make a get request to the weather API
     if (err) {
-      res.render('index', { weather: null, error: 'Oops! The weather man is on vacation. Please try again' });
+      res.render('index', { weather: null, error: 'Oops! The weather man is on vacation. Please try again', humidity: null });
     } else {
+      console.log(body);
       let weather = JSON.parse(body);
       if (weather.cod == '404') { //added condition if city is not found
-        res.render('index', { weather: null, error: 'Cannot find city on Earth' });
+        res.render('index', { weather: null, error: 'Cannot find city on Earth', humidity: null });
       }
       else if (weather.main != undefined) {
         let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-        res.render('index', { weather: weatherText, error: null });
+        let humidityText = `The humidity is ${weather.main.humidity} in ${weather.name}`;
+        if (req.body.weather != undefined) //if get weather
+          res.render('index', { weather: weatherText, error: null, humidity: null });
+        if (req.body.humidity != undefined) //if get humidity
+          res.render('index', { weather: null, error: null, humidity: humidityText });
       } else {
-        res.render('index', { weather: null, error: 'Oops! The weather man is on vacation. Please try again' });
+        res.render('index', { weather: null, error: 'Oops! The weather man is on vacation. Please try again', humidity: null });
       }
     }
   });
